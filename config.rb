2291c6ffe119a29f622b6772008592397071728c
James Anderson <jrsa@jrsa.co>
2016-06-12 11:06:03 -0700

    connect to s3 and grab list of everything in ze bucket. helper to filter this list and spit out images in the template

--- config.rb
+++ config.rb
@@ -1,3 +1,5 @@
+require 'aws/s3'
+
 ###
 # Page options, layouts, aliases and proxies
 ###
@@ -27,12 +29,29 @@ end
 # Helpers
 ###
 
+config[:bucket_name] = 'jrsaco'
+
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  bucket_name = 'jrsaco'
+  # set these ENVs for connection
+  # TODO: properly handle error if these aren't here
+  id =  ENV['AMAZON_ACCESS_KEY_ID']
+  key = ENV['AMAZON_SECRET_ACCESS_KEY']
+
+  AWS::S3::Base.establish_connection!(access_key_id: id, secret_access_key: key)
+
+  def s3_imgs match
+    bucket = AWS::S3::Bucket.find(config[:bucket_name])
+
+    # return sub-array of all images containing 'match' 
+    bucket.objects.select{ |ob| ob.key[match] && ob.about[:content_type]['image'] }
+  end
+
+  def vimeo_player id
+    "https://player.vimeo.com/video/#{id}"
+  end
+end
 
 # Build-specific configuration
 configure :build do
