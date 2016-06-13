f68826ccda4b326fdfa6521273fe69ef83555177
James Anderson <jrsa@jrsa.co>
2016-06-13 12:56:10 -0700

    oogle ganalytics

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+var cwekhja = 'gtm-t3rc5w';
+(function(x,y,z,l,i) {
+    x[l] = x[l] || [];
+    x[l].push({
+        event: 'gtm.js',
+        'gtm.start': new Date().getTime()
+    });
+    var f = y.getElementsByTagName(z)[0];
+    var j = y.createElement(z), dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', cwekhja.toUpperCase());
