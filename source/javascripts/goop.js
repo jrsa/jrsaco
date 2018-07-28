8f01e8bcec6f290b57ccf44aa8ecd23b847a3e07
James Anderson <jrsa@jrsa.co>
2018-07-28 15:45:25 -0700

    move everything from fboutils into main file

--- source/javascripts/goop.js
+++ source/javascripts/goop.js
@@ -17,6 +17,17 @@ if (typeof Object.create !== 'function') {
         map: function (value, low1, high1, low2, high2) {
             return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
         },
+        createTexture: function (gl) {
+            var texture = gl.createTexture();
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+            gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
+            return texture;
+        }
     };
 
     var config = {
@@ -81,18 +92,90 @@ if (typeof Object.create !== 'function') {
                     gl.TEXTURE_2D, this.texture, 0);
 
                 // this shouldn't be here but it will be for now
-                this.bb = new px.BB(gl);
+                this.bb = Object.create(jarsGl.bb);
+                this.bb.allocate(gl);
             },
-            bind: function (gl, ) {
+            bind: function (gl) {
                 gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
             },
             draw: function(gl, pgm) {
-                this.bb.draw(pgm, this.texture);
+                this.bb.draw(gl, pgm, this.texture);
             },
             // this isn't used and shouldn't exist, the draw function should
             // just handle multiple arguments
             draw2: function (gl, pgm, texture2) {
-                this.bb.draw2(pgm, this.texture, texture2);
+                this.bb.draw2(gl, pgm, this.texture, texture2);
+            },
+        },
+        bb: {
+            allocate: function (gl) {
+                var initBuffer = function (gl, buf, dataset) {
+                    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+                    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(dataset),
+                        gl.STATIC_DRAW);
+                }
+                this.vert = gl.createBuffer();
+                initBuffer(gl, this.vert, [-1.0, 1.0,
+                0.0, -1.0, -1.0, 0.0,
+                1.0, 1.0,
+                0.0, 1.0, -1.0, 0.0]);
+
+                this.tex = gl.createBuffer();
+                initBuffer(gl, this.tex, [
+                0, 1,
+                0, 0,
+                1, 1,
+                1, 0]);
+
+                this.color = gl.createBuffer();
+                initBuffer(gl, this.color, [
+                1, 1, 1, 1,
+                1, 1, 1, 1,
+                1, 1, 1, 1,
+                1, 1, 1, 1]);
+            },
+            predraw: function (gl, pgm) {
+                var vertexPosAttrib, vertexColorAttrib, vertexTexAttrib;
+
+                //set up vertex attributes
+                gl.useProgram(pgm);
+                vertexPosAttrib = gl.getAttribLocation(pgm, 'pos');
+                gl.enableVertexAttribArray(vertexPosAttrib);
+
+                vertexColorAttrib = gl.getAttribLocation(pgm, 'color');
+                gl.enableVertexAttribArray(vertexColorAttrib);
+
+                vertexTexAttrib = gl.getAttribLocation(pgm, 'texcoord');
+                gl.enableVertexAttribArray(vertexTexAttrib);
+
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.color);
+                gl.vertexAttribPointer(vertexColorAttrib, 4,
+                    gl.FLOAT, false, 0, 0);
+
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.vert);
+                gl.vertexAttribPointer(vertexPosAttrib, 3,
+                    gl.FLOAT, false, 0, 0);
+
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.tex);
+                gl.vertexAttribPointer(vertexTexAttrib, 2,
+                    gl.FLOAT, false, 0, 0);
+            },
+            draw: function (gl, pgm, texture) {
+              this.predraw(gl, pgm);
+              gl.uniform1i(gl.getUniformLocation(pgm, "u_image"), 0);
+              gl.activeTexture(gl.TEXTURE0);
+              gl.bindTexture(gl.TEXTURE_2D, texture);
+              gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+            },
+            draw2: function (gl, pgm, texture1, texture2) {
+                this.predraw(gl, pgm);
+                gl.uniform1i(gl.getUniformLocation(pgm, "u_image"), 0);
+                gl.uniform1i(gl.getUniformLocation(pgm, "u_image2"), 1);
+                gl.activeTexture(gl.TEXTURE0);
+                gl.bindTexture(gl.TEXTURE_2D, texture1);
+                gl.activeTexture(gl.TEXTURE1);
+                gl.bindTexture(gl.TEXTURE_2D, texture2);
+                gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
             },
         },
     };
